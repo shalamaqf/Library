c6cc2aa1f27fe7cc025b9c40ecdc4f787999b169
Shalama <shalamafadillaa@gmail.com>
2025-09-24 13:43:53 +0700

    Create DOM elements for a form

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Create DOM elements
-const booksContainer = document.createElement ("div");
-const bookCard = document.createElement("div");
+const books_container = document.createElement ("div");
+const book_card = document.createElement("div");
 const title = document.createElement ("h3");
 const author = document.createElement ("p");
 const pages = document.createElement ("p");
 let isRead = document.createElement("button");
 let removeBook = document.createElement("button");
 let viewDetail = document.createElement("button");
+
+const form_container = document.createElement("div");
+const form_book = document.createElement("form");
+const form_title = document.createElement("h2");
+const title_container = document.createElement("div");
+const label_title = document.createElement("label");
+const input_title = document.createElement("input");
+const author_container = document.createElement("div");
+const label_author = document.createElement("label");
+const input_author = document.createElement("input");
+const pages_container = document.createElement("div");
+const label_pages = document.createElement("label");
+const input_pages = document.createElement("input");
