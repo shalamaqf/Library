e82e3399b8863aa2e77c377fab31e5e50be9c525
Shalama <shalamafadillaa@gmail.com>
2025-09-24 13:27:27 +0700

    Create DOM elements for book's information

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create DOM elements
+const booksContainer = document.createElement ("div");
+const bookCard = document.createElement("div");
+const title = document.createElement ("h3");
+const author = document.createElement ("p");
+const pages = document.createElement ("p");
+let isRead = document.createElement("button");
+let removeBook = document.createElement("button");
+let viewDetail = document.createElement("button");
